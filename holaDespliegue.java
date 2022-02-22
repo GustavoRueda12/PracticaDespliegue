0f67f9b19c10d35f55bc7222122742298416a08c
Gustavo <xgruerui953@ieshnosmachado.org>
2022-02-22 09:02:22 +0000

    Colores definidos

--- holaDespliegue.java
+++ holaDespliegue.java
@@ -1,5 +1,7 @@
 public class HolaDespliegue {
   public static void main (String[] args) {
     System.out.println("Hola, despliegue!");
+    String naranja = "\033[33m";
+    String azul = "\033[34m";
   }
 }
